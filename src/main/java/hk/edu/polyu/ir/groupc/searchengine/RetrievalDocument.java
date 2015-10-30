3208dcc562fd47a66896312d54890cd129b483e1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-10-30 21:32:30 +0800

    updated retrievalmodel interface

---
+++ src/main/java/hk/edu/polyu/ir/groupc/searchengine/RetrievalDocument.java
@@ -0,0 +1,10 @@
+package hk.edu.polyu.ir.groupc.searchengine;
+
+/**
+ * Created by beenotung on 10/30/15.
+ */
+public class RetrievalDocument {
+    public String documentId;
+    /*higher is better*/
+    public double score;
+}
--- src/main/java/hk/edu/polyu/ir/groupc/searchengine/RetrievalModel.java
+++ src/main/java/hk/edu/polyu/ir/groupc/searchengine/RetrievalModel.java
@@ -3,4 +3,5 @@ package hk.edu.polyu.ir.groupc.searchengine;
  * Created by beenotung on 10/23/15.
  */
 public interface RetrievalModel {
+    public RetrievalDocument[]search(String query);
 }
