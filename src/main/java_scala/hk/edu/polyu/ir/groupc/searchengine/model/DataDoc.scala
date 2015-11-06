c81a9a151ac0ea1251e1aceca0a3f88afb7a27e4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-11-06 18:09:01 +0800

    to implement read from file.txt and post.txt

---
+++ src/main/java_scala/hk/edu/polyu/ir/groupc/searchengine/model/DataDoc.scala
@@ -0,0 +1,8 @@
+package hk.edu.polyu.ir.groupc.searchengine.model
+
+/**
+ * Created by beenotung on 11/6/15.
+ */
+object DataDoc {
+
+}
--- src/main/java_scala/hk/edu/polyu/ir/groupc/searchengine/model/Query.scala
+++ src/main/java_scala/hk/edu/polyu/ir/groupc/searchengine/model/Query.scala
@@ -1,24 +1,28 @@
 package hk.edu.polyu.ir.groupc.searchengine.model
 
 import java.io.File
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 import scala.io.Source
 
 /**
  * Created by beenotung on 11/6/15.
  */
+@deprecated
 object QueryFactory {
+  def createFromFile(file: File): Iterator[Query] = {
+    Source.fromFile(file).getLines().map[Query] (createFromString)
+  }
+
   def createFromString(rawString: String): Query = {
     val id = rawString.substring(0, 3)
     val content = rawString.substring(4, rawString.length)
     new Query(id.trim, content.trim)
   }
-
-  def createFromFile(file: File): Iterator[Query] = {
-    Source.fromFile(file).getLines().map[Query] (createFromString)
-  }
 }
 
+
+@deprecated
 class Query(val id: String, val content: String) {
   override def toString: String = {
     /*    Map(
@@ -29,3 +33,36 @@ class Query(val id: String, val content: String) {
     s"$id $content"
   }
 }
+
+
+object Query {
+  private val lock = new ReentrantReadWriteLock()
+  private var rawStrings: List[String] = ???
+
+  def loadFile(file: File) = {
+    lock.writeLock().lock()
+    rawStrings = Source.fromFile(file).getLines().toList
+    lock.writeLock().unlock()
+  }
+
+  def clearFile() = {
+    lock.writeLock().lock()
+    rawStrings = ???
+    lock.writeLock().unlock()
+  }
+
+  def getId(index: Int): String = {
+    lock.readLock().lock()
+    val rawString = rawStrings(index)
+    lock.readLock().unlock()
+    rawString.substring(0,3)
+  }
+
+  def getContent(index: Int) = {
+    lock.readLock().lock()
+    val rawString = rawStrings(index)
+    lock.readLock().unlock()
+    rawString.substring(4, rawString.length)
+  }
+}
+
