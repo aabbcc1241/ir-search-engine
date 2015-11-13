d3f74f97bfdbeabe0e2263c370421e10f6e3cf49
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-11-13 11:43:08 +0800

    implemented result (write to file) part

--- src/main/java/hk/edu/polyu/ir/groupc/searchengine/Launcher.java
+++ src/main/java/hk/edu/polyu/ir/groupc/searchengine/Launcher.java
@@ -5,17 +5,21 @@ import comm.exception.EssentialFileNotFoundException;
 import comm.exception.RichFileNotFoundException;
 import hk.edu.polyu.ir.groupc.searchengine.model.datasource.DocFileFactory;
 import hk.edu.polyu.ir.groupc.searchengine.model.datasource.SearchResult;
+import hk.edu.polyu.ir.groupc.searchengine.model.datasource.SearchResultFactory;
 import hk.edu.polyu.ir.groupc.searchengine.model.datasource.TermInfoFactory;
 import hk.edu.polyu.ir.groupc.searchengine.model.query.QueryFactory;
 import hk.edu.polyu.ir.groupc.searchengine.model.query.RetrievalModel;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
-import static hk.edu.polyu.ir.groupc.searchengine.Debug.log;
+import static hk.edu.polyu.ir.groupc.searchengine.Debug.*;
 
 public abstract class Launcher {
+
+    private boolean inited = false;
 
     public abstract String FILE_PATH();
 
@@ -26,20 +30,31 @@ public abstract class Launcher {
     public abstract String STOP_PATH();
 
     public abstract String JUDGEROBUST();
+
     public abstract String QUERY();
 
-    public void start(RetrievalModel retrievalModel) {
+    public void start(RetrievalModel retrievalModel, String resultFilename) {
         try {
             init();
-            run(retrievalModel);
+            log("running retrieval model: " + retrievalModel.getClass().getName());
+            List<SearchResult> searchResults = run(retrievalModel);
+            log("saving search result to file <" + resultFilename + ">");
+            try {
+                SearchResultFactory.writeToFile(searchResults, resultFilename);
+            } catch (IOException e) {
+                exception(e);
+                loge("Error: Failed to save search result!\nPlease make sure you have write permission on '" + resultFilename + "'");
+            }
             deinit();
         } catch (EssentialFileNotFoundException e) {
-//            exception(e);
-            log("Error: Please make sure you have '" + e.path + "'");
+            exception(e);
+            loge("Error: Please make sure you have '" + e.path + "'");
         }
     }
 
     private void init() throws EssentialFileNotFoundException {
+        if (inited) return;
+        inited = true;
         try {
             log("loading file-doc list");
             DocFileFactory.load(new File(FILE_PATH()));
@@ -62,8 +77,12 @@ public abstract class Launcher {
     }
 
     protected List<SearchResult> run(RetrievalModel retrievalModel) {
-        List<SearchResult> searchResults = new ArrayList<>();
-        Utils.foreach(QueryFactory.getQueries(), query -> searchResults.add(retrievalModel.search(query)));
+        List<SearchResult> searchResults = new LinkedList<>();
+        Utils.foreach(QueryFactory.getQueries(), query -> {
+            log("searching on queryId: " + query.queryId());
+            searchResults.add(retrievalModel.search(query));
+            log("finished search");
+        });
         return searchResults;
     }
 
--- src/main/java/hk/edu/polyu/ir/groupc/searchengine/model/datasource/SearchResult.scala
+++ src/main/java/hk/edu/polyu/ir/groupc/searchengine/model/datasource/SearchResult.scala
@@ -1,27 +1,37 @@
 package hk.edu.polyu.ir.groupc.searchengine.model.datasource
 
-import hk.edu.polyu.ir.groupc.searchengine.model.query.RetrievalDocument
+import java.io.{FileWriter, IOException}
+
+import comm.FileUtils
+import hk.edu.polyu.ir.groupc.searchengine.model.query.{Query, RetrievalDocument}
+
+import scala.collection.JavaConverters._
 
 /**
   * Created by beenotung on 11/6/15.
   */
 object SearchResultFactory {
-  var runId: String = ???
-  var queryId: String = ???
+  var runId: String = "runId_default"
 
   def setRunId(newRunId: String) = {
     runId = newRunId
   }
 
-  def setQueryId(newQueryId: String) = {
-    queryId = newQueryId
-  }
-
-  def create(retrievalDocuments: java.util.List[RetrievalDocument]): SearchResult =
-    new SearchResult(runId, queryId, retrievalDocuments)
+  def create(query: Query, retrievalDocuments: java.util.List[RetrievalDocument]): SearchResult =
+    create(query.queryId, retrievalDocuments)
 
   def create(queryId: String, retrievalDocuments: java.util.List[RetrievalDocument]): SearchResult =
     new SearchResult(runId, queryId, retrievalDocuments)
+
+  @throws(classOf[IOException])
+  def writeToFile(searchResults: java.util.List[SearchResult], filename: String): Unit = writeToFile(searchResults.asScala, filename)
+
+
+  @throws(classOf[IOException])
+  def writeToFile(searchResults: Seq[SearchResult], filename: String): Unit = {
+    val lines = searchResults.flatMap(sr => sr.toStrings)
+    FileUtils.appendToFile(lines, filename)
+  }
 }
 
 
@@ -31,5 +41,17 @@ class SearchResult(val runId: String, val queryId: String, val retrievalDocument
       val doc = retrievalDocuments.get(i)
       queryId + " " + doc.documentId + " " + (i + 1) + " " + doc.similarityScore + " " + runId
     })
+  }
+
+  @throws(classOf[IOException])
+  def writeToFile(filename: String) = {
+    val fw = new FileWriter(filename)
+    try {
+      fw.write("\n")
+      toStrings.foreach(s => fw.write(s + "\n"))
+    } catch {
+      case e: IOException => fw.close()
+        throw e
+    }
   }
 }
